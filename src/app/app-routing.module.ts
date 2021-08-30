400b92900ebb469781681dccce2335f4c8d43ac7
Douglas Shibata <douglas.shibata11@gmail.com>
2021-08-30 10:01:30 -0300

    styling access denied page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./layout/layout.module').then((m) => m.LayoutModule),
-        canActivate: [AuthGuard]
+        // canActivate: [AuthGuard]
     },
     { path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) },
     { path: 'signup', loadChildren: () => import('./signup/signup.module').then((m) => m.SignupModule) },
@@ -23,7 +23,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', useHash: true })],
     exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
